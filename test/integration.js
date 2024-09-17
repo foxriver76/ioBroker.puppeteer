53c99d75f0bfe132399dfec2c72ea618796aa6db
foxriver76 <moritz.heusinger@gmail.com>
2024-09-17 16:18:40 +0200

    fix test imports

--- test/integration.js
+++ test/integration.js
@@ -1,5 +1,6 @@
-import path from 'path';
 import { tests } from '@iobroker/testing';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.integration(path.join(__dirname, '..'));
+tests.integration(join(dirname(fileURLToPath(import.meta.url)), '..'));
--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,6 @@
-import path from 'path';
 import { tests } from '@iobroker/testing';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, '..'));
+tests.unit(join(dirname(fileURLToPath(import.meta.url)), '..'));
